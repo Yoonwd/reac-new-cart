cbbaf8882f254a5137663c070a1f2f1301f4f34c
You sik Jeon <andrewskej@gmail.com>
2018-11-10 15:54:13 +0000

    App.js edited online with Bitbucket

--- src/App.js
+++ src/App.js
@@ -18,6 +18,20 @@ class App extends Component {
     });
   }
 
+
+  componentDidUpdate(prevProps, prevState){ //update 가 있을때마다 발동하는 함수
+    console.log(prevState.items) //한 턴 전의 카트상태
+    console.log(this.state.items) //현재 카트 상태
+    if(prevState.items !== this.state.items){  //카트 내의 물건에 변화가 생기면(넣거나, 빼거나)
+      console.log(this.state.items) // 카트 내의 물건들 확인해보고
+      this.state.items.map(el => console.log(el.price) ) //카트 내의 물건들의 가격들 확인
+      var fp=0;   //최종가
+      this.state.items.forEach(el => fp += el.price )  //최종가는 현재 카트 내의 물건들.price 를 전부 더한 누적값(+= 로 구현)
+      console.log('fp',fp)  //최종가: 현재 카트 내의 물건들 가격의 총합
+      this.setState({total: fp}) // 최종가를 반영해 준다
+    }
+  }
+
   // 카트에서 삭제하는 함수
   onRemoveFromCart = this.onRemoveFromCart.bind(this);
   onRemoveFromCart(i) {
@@ -30,12 +44,12 @@ class App extends Component {
   } 
 
   // 카트 합계를 계산하는 함수
-  calculateTotal = this.calculateTotal.bind(this);
-  calculateTotal(price) {
-    this.setState({
-      total: [...this.state.total + price]
-    })
-  }
+  // calculateTotal = this.calculateTotal.bind(this);
+  // calculateTotal(price) {
+  //   this.setState({
+  //     total: [...this.state.total + price]
+  //   })
+  // }
 
   render() {
     return (
@@ -45,11 +59,11 @@ class App extends Component {
           total: this.state.total,
           onAddToCart: this.onAddToCart,
           onRemoveFromCart: this.onRemoveFromCart,
-          calculateTotal: this.calculateTotal
         }}
       >
         <div className="App">
           <Cart />
+          total:{this.state.total}  
           <List />
         </div>
       </CartContext.Provider>
